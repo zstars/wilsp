c9c53da1afdde6fa63e7e0d6a66e1c7322dcf685
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2016-04-22 18:15:33 +0200

    Moved files to /server

--- server/app/main/views.py
+++ server/app/main/views.py
@@ -1,8 +1,8 @@
-
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
 from . import main
 
+
 @main.route('/')
-def index()
+def index():
     return render_template('base.html')

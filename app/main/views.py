00e6cd2b58672124d14aeb2eb533557c6f7b2b88
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2016-04-22 17:47:40 +0200

    Added basic structure

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,8 @@
 
+from datetime import datetime
+from flask import render_template, session, redirect, url_for
+from . import main
+
+@main.route('/')
+def index()
+    return render_template('base.html')

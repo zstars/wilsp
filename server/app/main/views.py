e983898473969527ad5b2209f5d11dee4b470c83
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2016-04-27 18:22:31 +0200

    Proof-of-concept working

--- server/app/main/views.py
+++ server/app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, make_response, Response
 from . import main
 from app import rdb
 
@@ -16,6 +16,15 @@ def cam(cam_id):
     :param cam_id:
     :return:
     """
-    pass
+    alive = rdb.get("wilsa:feeder:alive")
+    if alive is None:
+        return "Feeder is down. Cam id: " + cam_id
+
+    cam_key = "wilsa:cams:" + cam_id + ":lastframe"
+    frame = rdb.get(cam_key)
+    if frame is None:
+        return "No frame available for: " + cam_key
+    else:
+        return Response(frame, status=200, mimetype="image/jpeg")
 
 

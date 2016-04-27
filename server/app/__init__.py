9078d84a6674858058106eef61f9342e0a15c6cb
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2016-04-27 18:08:56 +0200

    Started adding /cam url to server

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,15 +3,18 @@ from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
+from flask.ext.redis import FlaskRedis
 from flask.ext.sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+
 from config import config
 
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
-db = SQLAlchemy()
-
+# db = SQLAlchemy()
+rdb = None
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -21,6 +24,12 @@ def create_app(config_name):
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
-    db.init_app(app)
+    # db.init_app(app)
+    global rdb
+    rdb = FlaskRedis.from_custom_provider(StrictRedis, app)
+    rdb.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
--- server/app/main/views.py
+++ server/app/main/views.py
@@ -1,8 +1,21 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, current_app
 from . import main
+from app import rdb
 
 
 @main.route('/')
 def index():
     return render_template('base.html')
+
+
+@main.route('/cam/<cam_id>')
+def cam(cam_id):
+    """
+    Returns the image for the specified camera.
+    :param cam_id:
+    :return:
+    """
+    pass
+
+

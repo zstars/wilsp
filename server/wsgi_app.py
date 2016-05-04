d7bf71625dd9efca8d842deab006205e25a7541a
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2016-05-04 15:49:54 +0200

    Added wsgi_app file

---
+++ server/wsgi_app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from app import create_app
+
+WILSASERVER_DIR = os.path.dirname(__file__)
+if WILSASERVER_DIR == '':
+    WILSASERVER_DIR = os.path.abspath('.')
+
+sys.path.insert(0, WILSASERVER_DIR)
+os.chdir(WILSASERVER_DIR)
+
+sys.stdout = open('stdout.txt', 'w', 0)
+sys.stderr = open('stderr.txt', 'w', 0)
+
+
+application = create_app('deployment')
+
+import logging
+file_handler = logging.FileHandler(filename='errors.log')
+file_handler.setLevel(logging.INFO)
+application.logger.addHandler(file_handler)

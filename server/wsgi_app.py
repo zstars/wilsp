5c31c0d7fe771a84b70c5644e3edacdac37589e8
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2016-05-04 15:58:40 +0200

    Yet another fix

--- server/wsgi_app.py
+++ server/wsgi_app.py
@@ -16,7 +16,7 @@ sys.stdout = open('stdout.txt', 'w', 100)
 sys.stderr = open('stderr.txt', 'w', 100)
 
 
-application = create_app('deployment')
+application = create_app('ProductionConfig')
 
 import logging
 file_handler = logging.FileHandler(filename='errors.log')

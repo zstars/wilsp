05a5d264b67fdf688d41e9b2c42d7e3b497483bc
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2016-05-10 15:42:11 +0200

    Added new ImageRefreshCamFeeder and an ImageRefreshCamFeederTest (89% cov).

--- feeder/src/camfeeder/MJPEGCamFeeder.py
+++ feeder/src/camfeeder/MJPEGCamFeeder.py
@@ -1,4 +1,14 @@
-from camfeeder.old.CamFeeder import CamFeeder
+import gevent
+import grequests
+import redis
+import time
+
+from camfeeder.CamFeeder import CamFeeder
+
+
+class FrameGrabbingException(Exception):
+    def __init__(self, msg, cause=None):
+        super().__init__(msg, cause)
 
 
 class MJPEGCamFeeder(CamFeeder):
@@ -7,6 +17,52 @@ class MJPEGCamFeeder(CamFeeder):
     Most IP cameras (such as most Logitech models) provide MJPEG streams at particular URLs.
     """
 
-    def __init__(self, rdb, redis_prefix, cam_name, url, rotation=None):
-        super(MJPEGCamFeeder, self).__init__(rdb, redis_prefix, cam_name, url, rotation)
+    def __init__(self, rdb: redis.StrictRedis, redis_prefix: str, cam_name: str, url: str, max_fps: int,
+                 rotation: float = None):
+        super(MJPEGCamFeeder, self).__init__(rdb, redis_prefix, cam_name, url, max_fps, rotation)
 
+    def _run_until_inactive(self):
+        """
+        Will just keep pushing images and checking the active status until
+        the camera should not be active anymore.
+        :return:
+        """
+        while self._active:
+
+            update_start_time = time.time()
+
+            frame = self._grab_frame()
+            frame = self._rotated(frame, self._rotation)
+            self._put_frame(frame)
+
+            self._check_active()
+
+            elapsed = time.time() - update_start_time
+            intended_period = 1 / self._max_fps  # That's the approximate time a frame should take.
+
+            time_left = intended_period - elapsed
+            if time_left < 0:
+                # We are simply not managing to keep up with the intended frame rate, but for this
+                # cam feeder, that is not a (big) problem.
+                gevent.sleep(0)
+            else:
+                gevent.sleep(time_left)
+
+    def _grab_frame(self) -> bytes:
+        """
+        Grabs a frame. It will use the specified URL. Some special protocols may eventually be supported.
+        :return:
+        """
+        try:
+            rs = [grequests.get(self._url, stream=True)]
+            r = grequests.map(rs)[0]
+            if r.status_code != 200:
+                raise FrameGrabbingException("Status code is not 200")
+            content = r.content
+            if len(content) < 100:
+                raise FrameGrabbingException("Retrieved content is too small")
+            return content
+        except FrameGrabbingException:
+            raise
+        except Exception as exc:
+            raise FrameGrabbingException("Exception occurred", exc)
---
+++ feeder/test/camfeeder/ImageRefreshCamFeederTest.py
@@ -0,0 +1,134 @@
+import os
+import hashlib
+from unittest.mock import patch
+
+import gevent
+import grequests
+import io
+import redis
+import time
+
+import requests
+from mockredis import mock_strict_redis_client
+from FeederTestBase import FeederTestBase
+from camfeeder.CamFeeder import CamFeeder
+
+# Fix the working path
+from camfeeder.ImageRefreshCamFeeder import ImageRefreshCamFeeder, FrameGrabbingException
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(os.path.join(dname, '..'))
+
+
+class TestBasic(FeederTestBase):
+
+    def setUp(self):
+        self.rdb = mock_strict_redis_client()
+        self.img = open('data/img.jpg', 'rb').read()
+        self.cf = ImageRefreshCamFeeder(self.rdb, 'wilsat', 'archimedes', 'http://fake.com/image.jpg', 10, 0)
+
+    def tearDown(self):
+        pass
+
+    @patch('grequests.get')
+    @patch('grequests.map')
+    def test_grab_frame(self, map_patch, get_patch):
+
+        # Mock grequests get
+        response = requests.Response()
+        response.status_code = 200
+        get_patch.return_value = response
+
+        # Mock grequests map
+        response = requests.Response()
+        response.status_code = 200
+        response.raw = io.BytesIO(b"1234567890"*12)
+        map_patch.return_value = [response]
+
+        frame = self.cf._grab_frame()
+        self.assertIsNotNone(frame)
+        self.assertGreater(len(frame), 3)
+        self.assertTrue(frame.startswith(b'1234567890'))
+
+    @patch('grequests.get')
+    @patch('grequests.map')
+    def test_frame_grabbing_exception(self, map_patch, get_patch):
+
+        # Mock grequests get
+        response = requests.Response()
+        response.status_code = 500
+        get_patch.return_value = response
+
+        # Mock grequests map
+        response = requests.Response()
+        response.status_code = 500
+        response.raw = io.BytesIO(b"1234567890"*12)
+        map_patch.return_value = [response]
+
+        try:
+            frame = self.cf._grab_frame()
+            self.assertIsNotNone(frame)
+            self.assertGreater(len(frame), 3)
+            self.assertTrue(frame.startswith(b'1234567890'))
+        except FrameGrabbingException as fge:
+            pass
+        else:
+            self.fail('Expected a FrameGrabbingException')
+
+
+class TestRun(FeederTestBase):
+    """
+    Tests by letting the gevent greenlets run.
+    """
+
+    def setUp(self):
+        self.rdb = mock_strict_redis_client()
+        self.img = open('data/img.jpg', 'rb').read()
+        self.cf = ImageRefreshCamFeeder(self.rdb, 'wilsat', 'archimedes', 'http://fake.com/image.jpg', 10000, 0)
+
+        # Start running the greenlet.
+        self.cf.start()
+        self._g = self.cf._g
+
+    @patch('grequests.get')
+    @patch('grequests.map')
+    def test_active_flow(self, map_patch, get_patch):
+        """
+        Ensures that it seems to request frames and that it puts them on redis as expected.
+        :return:
+        """
+
+        # Mock grequests get
+        response = requests.Response()
+        response.status_code = 200
+        get_patch.return_value = response
+
+        # Mock grequests map
+        response = requests.Response()
+        response.status_code = 200
+        response.raw = io.BytesIO(b"1234567890"*12)
+        map_patch.return_value = [response]
+
+        # Should activate
+        self.rdb.setex('wilsat:cams:archimedes:active', 10, 1)
+        gevent.sleep(0.1)
+
+        # Get current number of frames
+        frames_first = self.cf._frames_this_cycle
+
+        # Let it run
+        gevent.sleep(0.1)
+
+        # Ensure that the number of frames has incrased
+        frames_second = self.cf._frames_this_cycle
+        self.assertGreater(frames_second, frames_first)
+
+        # Ensure that mock has been called several times.
+        self.assertGreater(map_patch.call_count, 5)
+
+        # Ensure that the frames are being placed into redis.
+        self.assertEquals(self.rdb.get('wilsat:cams:archimedes:lastframe'), b'1234567890'*12)
+
+    def tearDown(self):
+        gevent.kill(self._g)

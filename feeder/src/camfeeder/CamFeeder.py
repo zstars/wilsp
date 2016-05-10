9898df1762b6d7a7439fd1083c0a14b6ec4486e8
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2016-05-10 14:16:28 +0200

    Added somewhat thorough tests for the CamFeeder base class

--- feeder/src/camfeeder/CamFeeder.py
+++ feeder/src/camfeeder/CamFeeder.py
@@ -3,6 +3,7 @@ from abc import abstractmethod
 import gevent
 import io
 import redis
+import time
 from PIL import Image
 
 
@@ -17,10 +18,13 @@ class FrameGrabbingException(Exception):
 
 
 class CamFeeder(object):
-    TARGET_FPS = 150
-    ACTIVE_CHECK_PERIOD = 0.01  # Check if webcam is active every X seconds.
-    SLEEP_TIME_INACTIVE = 0.01  # Wait for x seconds after checking and being found inactive.
-    DEBUG = True
+    """
+    CamFeeder abstract base class. Children CamFeeders should at least implement the _run_until_inactive method.
+    The base class handles FSP control and activity/inactivity flow, as long as _check_active() is periodically called.
+    """
+
+    IMAGE_EXPIRE_TIME = 10
+    SLEEP_WHEN_INACTIVE = 0.01
 
     ########################################################
     # PUBLIC API
@@ -28,7 +32,7 @@ class CamFeeder(object):
 
     def __init__(self, rdb: redis.StrictRedis, redis_prefix: str, cam_name: str, url: str, max_fps: int,
                  rotation: float = None):
-        self._g = None
+        self._g = None  # type: gevent.Greenlet
         self._rdb = rdb  # type: redis.StrictRedis
         self._redis_prefix = redis_prefix
         self._url = url
@@ -41,12 +45,23 @@ class CamFeeder(object):
         self._active_since = None  # Timestamp when we last became active
 
     def get_current_fps(self) -> float:
-        raise NotImplementedError()
+        """
+        Retrieves the current FPS for this active cycle, measured as the number
+        of frames rendered so far in the cycle divided by the elapsed active time.
+        :return:
+        """
+        elapsed = time.time() - self._active_since
+        if elapsed == 0:
+            return 0
+        return self._frames_this_cycle / elapsed
 
     def start(self):
-        self._g = gevent.Greenlet(self.run)
+        """
+        Starts running the greenlet.
+        :return:
+        """
+        self._g = gevent.Greenlet(self._run)
         self._g.start()
-
 
     ########################################################
     # PRIVATE API
@@ -66,29 +81,40 @@ class CamFeeder(object):
         whether someone has started using the webcam.
         :return:
         """
-        raise NotImplementedError()
+        while not self._active:
+            self._check_active()
+            if not self._active:
+                gevent.sleep(CamFeeder.SLEEP_WHEN_INACTIVE)
 
     def _run(self) -> None:
         """
-        Greenlet's main thread. Will loop forever between the active and inactive loops.
+        Greenlet's main thread. Will loop forever between the active and inactive loops. Does not add sleep times.
         :return:
         """
-        raise NotImplementedError()
+        while True:
+            if self._active:
+                # We are becoming active.
+                self._frames_this_cycle = 0
+                self._active_since = time.time()
+                self._run_until_inactive()
+            if not self._active:
+                self._wait_until_active()
 
     def _check_active(self) -> None:
         """
         Checks whether we should change our activity status, and thus change the current mode.
         :return:
         """
-        raise NotImplementedError()
+        active = self._rdb.get("{}:cams:{}:active".format(self._redis_prefix, self._cam_name))
+        self._active = active is not None
 
-    def _notify_frame_put(self):
+    def _notify_frame_put(self) -> None:
         """
         Should be called just after a new frame is put into redis so that internal FPS calculations, etc,
         can be carried out automatically.
         :return:
         """
-        raise NotImplementedError()
+        self._frames_this_cycle += 1
 
     @staticmethod
     def _rotated(data: bytes, rotation: float) -> bytes:
@@ -112,7 +138,16 @@ class CamFeeder(object):
         return data
 
     def _put_frame(self, frame: bytes) -> None:
-        raise NotImplementedError()
+        """
+        Stores the specified frame into redis.
+        :param frame: Frame binary contents (full image)
+        :return:
+        """
+
+        # Set a relatively early expire to ensure that wrong images do not stay for long
+        self._rdb.setex("{}:cams:{}:lastframe".format(self._redis_prefix, self._cam_name), CamFeeder.IMAGE_EXPIRE_TIME, frame)
+
+        self._notify_frame_put()
 
 
 if __name__ == "__main__":
--- feeder/test/camfeeder/CamFeederTest.py
+++ feeder/test/camfeeder/CamFeederTest.py
@@ -1,6 +1,8 @@
 import os
 import hashlib
 
+import gevent
+import redis
 from mockredis import mock_strict_redis_client
 from FeederTestBase import FeederTestBase
 from camfeeder.CamFeeder import CamFeeder
@@ -14,9 +16,9 @@ os.chdir(os.path.join(dname, '..'))
 class TestBasic(FeederTestBase):
 
     def setUp(self):
-        rdb = mock_strict_redis_client()
+        self.rdb = mock_strict_redis_client()
         self.img = open('data/img.jpg', 'rb').read()
-        self.cf = CamFeeder(rdb, 'wilsat', 'archimedes', '', 10, 0)
+        self.cf = CamFeeder(self.rdb, 'wilsat', 'archimedes', '', 10, 0)
 
     def tearDown(self):
         pass
@@ -29,3 +31,84 @@ class TestBasic(FeederTestBase):
         r_md5 = hashlib.md5(r).hexdigest()
         self.assertIsNotNone(r)
         self.assertEquals('d27e3eea81060a69e9d62d2343fe5bcb', r_md5)
+
+    def test_put_frame(self):
+        self.cf._frames_this_cycle = 0
+        self.assertEquals(0, self.cf._frames_this_cycle)
+        frame = b'abcd'
+        self.cf._put_frame(frame)
+        self.assertEquals(frame, self.rdb.get('wilsat:cams:archimedes:lastframe'))
+        self.assertEquals(1, self.cf._frames_this_cycle)
+
+    def test_check_active(self):
+        # Set active flag:
+        self.rdb.setex('wilsat:cams:archimedes:active', 10, 1)
+        self.cf._check_active()
+        self.assertTrue(self.cf._active)  # We are marked as active
+
+        self.rdb.delete('wilsat:cams:archimedes:active')
+        self.cf._check_active()
+        self.assertFalse(self.cf._active)  # We are marked as inactive
+
+
+class ConcreteCamFeeder(CamFeeder):
+    """
+    Concrete CamFeeder for testing.
+    """
+
+    def __init__(self, rdb: redis.StrictRedis, redis_prefix: str, cam_name: str, url: str, max_fps: int,
+                 rotation: float = None):
+        super().__init__(rdb, redis_prefix, cam_name, url, max_fps, rotation)
+        self._times_active = 0
+        self._times_inactive = 0
+
+    def _run_until_inactive(self):
+        self._times_active += 1
+        while self._active:
+            self._check_active()
+            gevent.sleep(0.01)
+
+    def _wait_until_active(self):
+        self._times_inactive += 1
+        super()._wait_until_active()
+
+
+class TestRun(FeederTestBase):
+
+    def setUp(self):
+        self.rdb = mock_strict_redis_client()
+        self.img = open('data/img.jpg', 'rb').read()
+        self.cf = ConcreteCamFeeder(self.rdb, 'wilsat', 'archimedes', '', 10, 0)
+
+        # Start running the greenlet.
+        self.cf.start()
+        self._g = self.cf._g
+
+    def test_active_flow(self):
+        """
+        Ensures that it switches between the active and inactive states predictably,
+        and that the _wait_until_active and _run_until_inactive execute as they should.
+        :return:
+        """
+
+        # Give it some execution time.
+        gevent.sleep(0.1)
+
+        # Should start inactive.
+        self.assertEquals(0, self.cf._times_active)
+        self.assertEquals(1, self.cf._times_inactive)
+
+        # Should activate
+        self.rdb.setex('wilsat:cams:archimedes:active', 10, 1)
+        gevent.sleep(0.1)
+        self.assertEquals(1, self.cf._times_active)
+        self.assertEquals(1, self.cf._times_inactive)
+
+        # Should deactivate again
+        self.rdb.delete('wilsat:cams:archimedes:active')
+        gevent.sleep(0.1)
+        self.assertEquals(1, self.cf._times_active)
+        self.assertEquals(2, self.cf._times_inactive)
+
+    def tearDown(self):
+        gevent.kill(self._g)

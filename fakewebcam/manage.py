dbb233c25bf0611c57d74614ddf073deb7bb73a3
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2017-03-17 13:00:57 +0100

    Removed socketio dependency from fake webcam

--- fakewebcam/manage.py
+++ fakewebcam/manage.py
@@ -11,21 +11,6 @@ manager = Manager(app)
 def make_shell_context():
     return dict(app=app)
 
-@manager.command
-def run():
-   socketio.run(app,
-                host='127.0.0.1',
-                port=5000,
-                use_reloader=True)
-
-@manager.command
-def runprod():
-    socketio.run(app,
-                 host='127.0.0.1',
-                 port=8500,
-                 use_reloader=False,
-                 )
-
 manager.add_command("shell", Shell(make_context=make_shell_context()))
 
 if __name__ == '__main__':

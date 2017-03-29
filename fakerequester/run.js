9d5e4b765f0cec541e2e7b9d31ebe28901d4fde4
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2017-03-29 17:53:42 +0200

    Fix that pycharm had broken (import config)

--- fakerequester/run.js
+++ fakerequester/run.js
@@ -1,38 +1,32 @@
 var io = require('socket.io-client');
 
-console.log("Starting");
+var number = 1;
 
-var socket = io.connect('http://localhost:5000/h264');
+if(process.argv.length == 3) {
+    number = parseInt(process.argv[2]);
+}
 
-socket.on('connect', function () {
-    console.log("socket connected");
-
-    socket.emit('start', {'cam': 'cam0_0'});
-});
-
-socket.on('disconnect', function () {
-    console.log("socket disconnected");
-});
-
-socket.on('stream', function (arg) {
-    console.log('stream event received');
-});
+console.log("Starting for " + number.toString());
 
 
-var socket2 = io.connect('http://localhost:5000/h264');
+for(var i = 0; i < number; i++) {
+    (function() {
+        var socket = io.connect('http://localhost:5000/h264');
 
-socket2.on('connect', function () {
-    console.log("socket2 connected");
+        socket.on('connect', function () {
+            console.log("socket connected");
 
-    socket2.emit('start', {'cam': 'cam0_0'});
-});
+            socket.emit('start', {'cam': 'cam0_0'});
+        });
 
-socket2.on('disconnect', function () {
-    console.log("socket2 disconnected");
-});
+        socket.on('disconnect', function () {
+            console.log("socket disconnected");
+        });
 
-socket2.on('stream', function (arg) {
-    console.log('stream event received 2');
-});
+        socket.on('stream', function (arg) {
+            // console.log('stream event received');
+        });
+    })();
+}
 
 

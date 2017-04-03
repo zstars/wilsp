52bd7a1324c1f0fa67efee953dc8d0239d461de7
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2017-04-03 16:05:21 +0200

    FR runner can now run commands. (Though not yet the actual command)

--- server/benchmark/fr.py
+++ server/benchmark/fr.py
@@ -5,12 +5,20 @@ Fake Requester instances need to be started in a different computer, and on-dema
 managed using paramiko or fabric or similar.
 """
 
-from fabric.api import env,run,execute,hosts
+from fabric.api import env, run, execute, hosts
+
 
 def start_remote_fakerequester(host, keyfile):
     env.key_filename = keyfile
     execute(run_remote_commands, hosts=[host])
 
+
 def run_remote_commands():
     result = run("ls -l /var/www")
     print("RESULT: {}".format(result))
+
+
+# This is just for testing and development. This script is meant to be called
+# automatically from the benchmark.
+if __name__ == "__main__":
+    start_remote_fakerequester("weblab@plunder.weblab.deusto.es:5800", "/Users/lrg/.ssh/id_rsa")

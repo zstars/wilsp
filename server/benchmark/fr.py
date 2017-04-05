6642f2c4458fa0eb39c3da0640ca872d11d65907
Luis Rodriguez-Gil <luis.rodgi@gmail.com>
2017-04-05 19:06:48 +0200

    Fakerequester runner working though not necessarily stopping

--- server/benchmark/fr.py
+++ server/benchmark/fr.py
@@ -8,7 +8,10 @@ The remote computer needs to have everything ready:
 The fakerequester in a path, and node ready to use.
 """
 
-from fabric.api import env, run, execute, hosts, cd
+from fabric.api import env, run, execute, cd
+from fabric.network import ssh
+
+ssh.util.log_to_file("paramiko.log", 10)
 
 
 def start_remote_fakerequester(host, keyfile, path, clients):
@@ -21,18 +24,20 @@ def start_remote_fakerequester(host, keyfile, path, clients):
     :return:
     """
     env.key_filename = keyfile
+    env.gateway = "lrg@plunder.weblab.deusto.es:5800"
     execute(run_remote_commands, path, clients, hosts=[host])
 
 
 def run_remote_commands(path, clients):
     with cd(path):
-        run("node run.js -w {}".format(clients))
+        run("source ~/.bashrc && source ~/.nvm/nvm.sh && env && node run.js -w {}".format(clients))
+        # run("bash")
 
-def run_test_command():
-    result = run("ls -l /var/www")
+def run_test_command(path, clients):
+    result = run("pwd")
     print("RESULT: {}".format(result))
 
 # This is just for testing and development. This script is meant to be called
 # automatically from the benchmark.
 if __name__ == "__main__":
-    start_remote_fakerequester("weblab@plunder.weblab.deusto.es:5800", "/Users/lrg/.ssh/id_rsa", "/home/lrg/", 2)
+    start_remote_fakerequester("lrg@newplunder", "/Users/lrg/.ssh/id_rsa", "/home/lrg/wilsa/wilsaproxy/fakerequester", 2)

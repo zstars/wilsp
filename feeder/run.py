68d54aa024add38e980a049afcfca9c5e6a89db9
Luis Rodriguez-Gil <luis@labsland.com>
2018-12-11 23:16:09 +0100

    Should now be able to recover from redis shutdowns

--- feeder/run.py
+++ feeder/run.py
@@ -1,6 +1,8 @@
 import os
 import signal
 import sys
+import time
+import traceback
 
 import redis
 import yaml
@@ -40,7 +42,7 @@ def watchdog(rdb):
 
 
 def run():
-    global greenlets
+    global greenthreads
 
     print("Starting Feeder component", flush=True)
 
@@ -54,65 +56,86 @@ def run():
 
     # print("Loaded {}".format(config.CAMS_YML))
 
-    # Connect to the redis instance
-    rdb = redis.StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB, decode_responses=True)
+    while True:
+        try:
+            print("Running.")
 
-    # Clear keys so that the stats are right.
-    for key in rdb.scan_iter("{}:*".format(config.REDIS_PREFIX)):
-        # print("Deleting: {}".format(key))
-        rdb.delete(key)
+            # Connect to the redis instance
+            rdb = redis.StrictRedis(host=config.REDIS_HOST, port=config.REDIS_PORT, db=config.REDIS_DB, decode_responses=True)
 
-    # Create every cam feeder
-    for cam_name, cam in cams.items():
-        # print('Adding cam {0} to the dict'.format(cam_name))
-        if 'rotation' in cam:
-            rotation = float(cam['rotation'])
-        else:
-            rotation = 0.0
+            # Clear keys so that the stats are right.
+            for key in rdb.scan_iter("{}:*".format(config.REDIS_PREFIX)):
+                # print("Deleting: {}".format(key))
+                rdb.delete(key)
 
-        url = cam.get('img_url')
-        mjpeg_url = cam.get('mjpeg_url')
-        mpeg = cam.get('mpeg')
-        h264 = cam.get('h264')
-        h264_source = cam.get('h264_source')
+            # Create every cam feeder
+            for cam_name, cam in cams.items():
+                # print('Adding cam {0} to the dict'.format(cam_name))
+                if 'rotation' in cam:
+                    rotation = float(cam['rotation'])
+                else:
+                    rotation = 0.0
 
-        if mjpeg_url is not None:
-            cf = MJPEGCamFeeder(rdb, config.REDIS_PREFIX, cam_name, mjpeg_url, 30, rotation)
-        elif url is not None:
-            cf = ImageRefreshCamFeeder(rdb, config.REDIS_PREFIX, cam_name, url, 30, rotation)
-        elif h264_source is not None:
-            cf = H264ToFramesFeeder(rdb, config.REDIS_PREFIX, cam_name, h264_source, config.FFMPEG_BIN)
+                url = cam.get('img_url')
+                mjpeg_url = cam.get('mjpeg_url')
+                mpeg = cam.get('mpeg')
+                h264 = cam.get('h264')
+                h264_source = cam.get('h264_source')
 
-        if mjpeg_url is None and url is None and h264_source is None:
-            raise Exception("img_url or mjpeg_url or h264_source is not specified for camera {}".format(cam_name))
+                if mjpeg_url is not None:
+                    cf = MJPEGCamFeeder(rdb, config.REDIS_PREFIX, cam_name, mjpeg_url, 30, rotation)
+                elif url is not None:
+                    cf = ImageRefreshCamFeeder(rdb, config.REDIS_PREFIX, cam_name, url, 30, rotation)
+                elif h264_source is not None:
+                    cf = H264ToFramesFeeder(rdb, config.REDIS_PREFIX, cam_name, h264_source, config.FFMPEG_BIN)
 
-        if mpeg is not None and mpeg is True:
-            mpeg_cf = MPEGFeeder(rdb, cam_name, mjpeg_url, config.FFMPEG_BIN)
-            cam_feeders[cam_name + '/mpeg'] = mpeg_cf
-            mpeg_cf.start()
+                if mjpeg_url is None and url is None and h264_source is None:
+                    raise Exception("img_url or mjpeg_url or h264_source is not specified for camera {}".format(cam_name))
 
-        if h264 is not None and h264 is True:
-            h264_cf = H264Feeder(rdb, config.REDIS_PREFIX, cam_name, mjpeg_url, config.FFMPEG_BIN)
-            cam_feeders[cam_name + '/h264'] = h264_cf
-            h264_cf.start()
+                if mpeg is not None and mpeg is True:
+                    mpeg_cf = MPEGFeeder(rdb, cam_name, mjpeg_url, config.FFMPEG_BIN)
+                    cam_feeders[cam_name + '/mpeg'] = mpeg_cf
+                    mpeg_cf.start()
 
-        cam_feeders[cam_name] = cf
-        cf.start()
+                if h264 is not None and h264 is True:
+                    h264_cf = H264Feeder(rdb, config.REDIS_PREFIX, cam_name, mjpeg_url, config.FFMPEG_BIN)
+                    cam_feeders[cam_name + '/h264'] = h264_cf
+                    h264_cf.start()
 
-    # Create the watchdog
-    g = gevent.spawn(watchdog, rdb)
-    greenthreads.append(g)
+                cam_feeders[cam_name] = cf
+                cf.start()
 
-    # Wait for all the greenlets
-    cam_feeder_greenthreads_list = [cam._g for cam in cam_feeders.values()]
+            # Create the watchdog
+            g = gevent.spawn(watchdog, rdb)
+            greenthreads.append(g)
 
-    for cam_feeder_greenthreads in cam_feeder_greenthreads_list:
-        greenthreads.extend(cam_feeder_greenthreads)
+            # Wait for all the greenlets
+            cam_feeder_greenthreads_list = [cam._g for cam in cam_feeders.values()]
+
+            for cam_feeder_greenthreads in cam_feeder_greenthreads_list:
+                greenthreads.extend(cam_feeder_greenthreads)
+
+            # Wait for greenlets
+            for g in greenthreads:
+                g.join()
+
+        except Exception as ex:
+            print("An exception was caught in the main function.")
+            traceback.print_exc()
+
+            print("Killing greenlets...")
+            try:
+                gevent.killall(greenthreads, block=True, timeout=30)
+            except:
+                traceback.print_exc()
+                print("Could not kill greenlets. Exiting the feeders.")
+                exit(1)
+            greenthreads = []
+
+            print("Retrying in 5 seconds...")
+            time.sleep(5)
+            print("Retrying now.", flush=False)
 
 
 if __name__ == '__main__':
     run()
-
-    # Wait for greenlets
-    for g in greenthreads:
-        g.join()
